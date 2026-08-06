c37acfd52f5bfff909afb69084c823bf8922571c
Sumayya Sayeed <Sumayya.Sayeed@nestgroup.net>
2026-08-06 19:56:15 +0530

    tf format

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,16 +1,16 @@
 variable "instance_name" {
   default     = "voting_app_ec2"
-    description = "Instance name"
+  description = "Instance name"
 }
 variable "instance_type" {
   default     = "t3.micro"
-    description = "ec2 instance type"
+  description = "ec2 instance type"
 }
 variable "key_name" {
   default     = "ec2-keypair"
-    description = "Key pair name" 
+  description = "Key pair name"
 }
 variable "allowed_ssh_cidr" {
-    default = "0.0.0.0/0"
-    description = "CIDR block for SSH access"
+  default     = "0.0.0.0/0"
+  description = "CIDR block for SSH access"
 }
