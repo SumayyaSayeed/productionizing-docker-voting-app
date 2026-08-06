ec112282cf07006cb797664850358624a2e5eed0
Sumayya Sayeed <Sumayya.Sayeed@nestgroup.net>
2026-08-06 20:07:01 +0530

    ec2 subnet

--- Infra/main.tf
+++ Infra/main.tf
@@ -5,7 +5,7 @@ module "ec2_instance" {
   name          = var.instance_name
   instance_type = var.instance_type
   key_name      = var.key_name
-
+  subnet_id = data.aws_subnets.default.ids[0]
   create_security_group        = true
   security_group_ingress_rules = local.security_group_ingress_rules
   user_data                    = local.user_data
