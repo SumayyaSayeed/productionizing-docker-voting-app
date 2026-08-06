429d462e06d2d0b4f7cf58ee3f36c5b0f265450f
Sumayya Sayeed <Sumayya.Sayeed@nestgroup.net>
2026-08-06 20:10:46 +0530

    ec2 subnet

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,10 +2,10 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 6.0"
 
-  name          = var.instance_name
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id = data.aws_subnets.default.ids[0]
+  name                         = var.instance_name
+  instance_type                = var.instance_type
+  key_name                     = var.key_name
+  subnet_id                    = data.aws_subnets.default.ids[0]
   create_security_group        = true
   security_group_ingress_rules = local.security_group_ingress_rules
   user_data                    = local.user_data
